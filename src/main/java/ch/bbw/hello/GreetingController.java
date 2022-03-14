b989182cfba14f90e3082d092201a9fb4437b0bd
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-03-14 16:02:18 +0100

    Init

---
+++ src/main/java/ch/bbw/hello/GreetingController.java
@@ -0,0 +1,24 @@
+package ch.bbw.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/greeting/{name}")
+    public String greeting(Model model, @PathVariable String name) { // parameter order swapped so greeting()s can coexist
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
